bedd3e988b2c24421a8cba8588929f29b40dbb2f
Supachai Vorapojpisut <vsupacha@gmail.com>
2024-01-27 20:53:22 +0700

    Add new project

--- examples/taist_jan_25/src/main.cpp
+++ examples/taist_jan_25/src/main.cpp
@@ -18,6 +18,7 @@ void sound_detect_task(void *pvParameter) {
   // prepare baseline with 100 rounds
   base_sample = 0;
   for (int i=0; i < 100; i++) {
+    num_samples = 160;
     hw_mic_read(samples, &num_samples);
     avg_sample = 0;
     for (int j=0; j < num_samples; j++) {
@@ -30,23 +31,50 @@ void sound_detect_task(void *pvParameter) {
 
   // read microphone
   while (true) {
+    num_samples = 160;
     hw_mic_read(samples, &num_samples);
     // do something with samples
-
+    avg_sample = 0;
+    for (int j=0; j < num_samples; j++) {
+      avg_sample += abs(samples[j]);
+    }
+    avg_sample /= num_samples;
+    avg_sample /= base_sample;
     // send event to comm task
-
+    xQueueSend(evt_queue, &avg_sample, portMAX_DELAY);
   }
 }
 
 // comm task
 void comm_task(void *pvParameter) {
   float value;
+  const int BUF_SIZE = 25;
+  float buf[BUF_SIZE];
+  int buf_idx = 0;
+  float avg_value;
+  bool detected = false;
+
   Serial.begin(115200);
+  for (int i=0; i < BUF_SIZE; i++) {
+    buf[i] = 0;
+  }
   while (true) {
     // wait for event
-
+    xQueueReceive(evt_queue, &value, portMAX_DELAY);
+    buf[buf_idx] = value; // update circular buffer
+    buf_idx = (buf_idx + 1) % BUF_SIZE;
+    avg_value = 0; // do moving average
+    for (int i=0; i < BUF_SIZE; i++) {
+      avg_value += buf[i];
+    }
+    avg_value /= BUF_SIZE;
+    if (avg_value > 0.5) { // do threshold
+      detected = true;
+    } else {
+      detected = false;
+    }
     // do something with event
-
+    Serial.printf("%d, %f, %d\n", millis(), value, detected);
   }
 }
 
